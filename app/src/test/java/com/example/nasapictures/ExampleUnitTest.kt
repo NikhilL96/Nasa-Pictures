1e544ac40d96e5c19c5af975e3cbdcc46cec7b50
NikhilL96 <nikhil96.mail@gmail.com>
2020-11-16 15:54:14 +0530

    Changed primaryColor and background color

--- app/src/test/java/com/example/nasapictures/ExampleUnitTest.kt
+++ app/src/test/java/com/example/nasapictures/ExampleUnitTest.kt
@@ -12,7 +12,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
